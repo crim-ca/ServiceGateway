ea1b909ad3db3afd2f0309014efaee9408341fee
Fred Osterrath <osterrfr@crim.ca>
2017-03-29 15:31:41 -0400

    Can use a task name on different queues

--- ServiceGateway/__meta__.py
+++ ServiceGateway/__meta__.py
@@ -2,6 +2,6 @@
 Meta values for package.
 """
 
-__version__ = '1.7.5'
+__version__ = '1.7.6'
 __author__ = "osterrfr@crim.ca"
 __contact__ = "vesta-support@crim.ca"
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('HISTORY.rst') as history_file:
     HISTORY = history_file.read().replace('.. :changelog:', '')
 
 REQUIREMENTS = ["python-novaclient==2.23.2",
-                "VestaRestPackage==1.7.2"]
+                "VestaRestPackage==1.7.4"]
 
 setup(
     # -- Meta information --------------------------------------------------
