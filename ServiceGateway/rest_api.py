98d56cdc9cfaf0192b73a56e6afdb4791eccd95b
Fred Osterrath <osterrfr@crim.ca>
2017-05-31 15:28:09 -0400

    Works : Pass whole JSON structure in misc but keep base parameters in URL

--- ServiceGateway/rest_api.py
+++ ServiceGateway/rest_api.py
@@ -45,31 +45,50 @@ TEMPLATES_LOADER = jinja2.ChoiceLoader([
 APP.jinja_loader = TEMPLATES_LOADER
 
 
-@APP.route("/<service_route>/submit_json", methods=['POST'])
-def submit_json(service_route):
+@APP.route("/<service_route>/process", methods=['POST'])
+@APP.route("/<service_route>/process/<storage_doc_id>", methods=['POST'])
+def process(service_route, storage_doc_id=None):
     """
-    POST a JSON structure to the service Gateway which will be passed on to the
-    service.
+    POST a JSON structure to the service Gateway which partial contents will be
+    passed on to the service.
+
+    Optionnal keys are the following:
+
+    :param storage_doc_id: The unique document ID of the file to transcode
+       which can be found on the associated MSS. If not provided, a doc_url
+       parameter must be submitted in the request
+    :param doc_url: Document on which the processing will take place. Can be
+       set to None if using the storage_doc_id argument or if there is no
+       actual processing on a document.
+    :param options: Will be used as a sub-structure which will be integrally
+       passed on to the processing worker.
     """
     logger = logging.getLogger(__name__)
+
+    validate_authorisation(request, APP.config["SECURITY"])
+
     logger.info("JSON structure submitted at %s", service_route)
     json_struct = request.get_json()
     logger.debug("JSON contents : %s", json_struct)
-    storage_doc_id = json_struct.pop('storage_doc_id')
-    ann_srv_url = json_struct.pop('ann_srv_url')
 
-    result = submit_task(storage_doc_id, 'annotate', service_route,
+    if 'ann_doc_id' in request.values:
+        ann_doc_id = request.values['ann_doc_id']
+        args = {'ann_doc_id': ann_doc_id}
+        ann_srv_url = get_request_url('POST_ANNOTATIONS_REQ_URL', args)
+        logger.info("Will submit annotations to %s", ann_srv_url)
+    else:
+        ann_srv_url = None
+
+    result = submit_task(storage_doc_id,
+                         'annotate',
+                         service_route,
                          ann_srv_url=ann_srv_url,
                          misc=json_struct)
     return result
 
 
-@APP.route("/<service_route>/process", methods=['POST'])
-@APP.route("/<service_route>/process/<storage_doc_id>",
-           methods=['POST'])
 @APP.route("/<service_route>/annotate", methods=['POST'])
-@APP.route("/<service_route>/annotate/<storage_doc_id>",
-           methods=['POST'])
+@APP.route("/<service_route>/annotate/<storage_doc_id>", methods=['POST'])
 def annotate(service_route, storage_doc_id=None):
     """
     POST a processing request through a form
